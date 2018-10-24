539cbc0e677e550aac16ac7fb90fc0787241fed4
alicravi <rizahawk@gmail.com>
2018-10-24 00:35:53 -0300

    How static and non-static methods works when it comes to objects

--- learning-java/src/static_methods/HelloWorld.java
+++ learning-java/src/static_methods/HelloWorld.java
@@ -2,9 +2,10 @@
 
 package static_methods;
 
+
 public class HelloWorld{
 	public static void main(String args[]) {
-		GoodbyeWorld.print();//no need to create an object since the method being called is static
+		GoodbyeWorld.print();//static method being called in a static way (no objects) 
 	}
 	
 	public void print() {
